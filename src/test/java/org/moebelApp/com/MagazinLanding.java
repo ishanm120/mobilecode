97145572040744df690112c6aaae3b120c0a29e8
ishanmittal120@gmail.com <Ishan@1506>
2017-09-15 13:20:25 +0530

    message added

--- src/test/java/org/moebelApp/com/MagazinLanding.java
+++ src/test/java/org/moebelApp/com/MagazinLanding.java
@@ -18,7 +18,7 @@ public class MagazinLanding extends Base {
 		WebElement magazinIcon= driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"moebel.de:id/tittleMagazine\")");
 		magazinIcon.click();
 		Thread.sleep(3000);
-		
+		System.out.println("test messgae to check for git");
 		WebElement catdropdown= driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"moebel.de:id/categoryLayout\")");
 		catdropdown.click();
 		Thread.sleep(5000);
